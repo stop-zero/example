23099265f3f4e3721852d46e8eef11fc16c38684
stop-zero <jungjy0097@naver.com>
2023-01-17 18:31:50 +0900

    first commit2

---
+++ if.py
@@ -0,0 +1,13 @@
+input_id=input('id: ')
+id='jiyoung'
+input_password=input('password:')
+
+password='010417'
+
+if input_id == id:
+    if input_password ==password:
+        print('Welcome!')
+    else:
+        print('Wrong password')
+else:
+    print('Wrong id')
